de8a2d88ba0dfcb55194ce5cc84b2e1135eedf83
Stefaan Ternier <stefaan.ternier@gmail.com>
2017-06-11 08:52:20 +0000

    set up rest server

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodejs_domintell
 Nodjs server that can sync with domintell DETH02 module
+
+#required npm modules
+npm install restify
+npm install firebase
--- js/restserver.js
+++ js/restserver.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+var restify = require('restify');
+
+var server = restify.createServer({
+    name: 'Nodejs Domintell gateway',
+    version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/echo/:name', function (req, res, next) {
+    res.send(req.params);
+    return next();
+});
+
+server.listen(8980, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
